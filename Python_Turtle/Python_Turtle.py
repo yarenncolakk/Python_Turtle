92305dffbc4f159473730ffe9bcb401daf1884a3
Yaren Colak <yarencolak1122@outlook.com>
2024-11-24 19:26:29 +0300

    Create Python_Turtle.py
    init

---
+++ Python_Turtle/Python_Turtle.py
@@ -0,0 +1,73 @@
+from turtle import *
+rafael=Turtle()
+rafael.shape("turtle")
+rafael.pensize(5)
+rafael.color("red")
+rafael.speed(2)
+rafael.begin_fill()
+rafael.left(90)
+rafael.fd(200)
+rafael.right(90)
+rafael.fd(200)
+rafael.right(90)
+rafael.fd(200)
+rafael.right(90)
+rafael.fd(350)
+rafael.end_fill()
+
+donatello=Turtle()
+donatello.shape("turtle")
+donatello.pensize(5)
+donatello.color("purple")
+donatello.begin_fill()
+donatello.speed(2)
+
+donatello.left(90)
+for i in range(4):
+ donatello.forward(150)
+ donatello.right(90)
+
+donatello.left(90)
+donatello.fd(120)
+donatello.end_fill()
+
+leonardo=Turtle()
+leonardo.shape("turtle")
+leonardo.pensize(5)
+leonardo.color("blue")
+leonardo.begin_fill()
+leonardo.speed(2)
+
+leonardo.left(90)
+for i in range(4):
+    leonardo.forward(100)
+    leonardo.right(90)
+
+leonardo.left(90)
+leonardo.fd(90)
+leonardo.end_fill()
+
+mikelanjelo=Turtle()
+mikelanjelo.shape("turtle")
+mikelanjelo.pensize(5)
+mikelanjelo.color("yellow")
+mikelanjelo.begin_fill()
+mikelanjelo.speed(2)
+
+mikelanjelo.left(90)
+for i in range(4):
+ mikelanjelo.forward(50)
+ mikelanjelo.right(90)
+
+mikelanjelo.left(90)
+mikelanjelo.fd(60)
+mikelanjelo.end_fill()
+
+
+
+
+
+ 
+ 
+
+
